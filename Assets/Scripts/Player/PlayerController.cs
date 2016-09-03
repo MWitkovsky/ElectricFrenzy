5165b62530dd15096feaa12c7502180ac01a823f
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-09-03 10:51:00 -0400

    added dashing

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,16 +5,68 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float dashTime = 0.15f;
+    [SerializeField]
+    private float dashCooldown = 0.1f;
 
     private Rigidbody2D rb;
+    private float attackTimer, dashTimer, dashCooldownTimer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if (dashTimer > 0.0f)
+        {
+            dashTimer -= Time.deltaTime;
+        }
+        else
+        {
+            if (rb.velocity.magnitude > moveSpeed)
+                rb.velocity = Vector2.zero;
+            if (dashCooldown > 0.0f)
+                dashCooldownTimer -= Time.deltaTime;
+        }
+            
+
+        if(attackTimer > 0.0f)
+            attackTimer -= Time.deltaTime;
+    }
+
 	public void Move(Vector2 move)
     {
-        transform.Translate(move * moveSpeed * Time.fixedDeltaTime);
+        if(AbleToMove())
+        {
+            rb.velocity = Vector2.zero;
+            transform.Translate(move * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void Dash(Vector2 move)
+    {
+        if (AbleToMove())
+        {
+            dashTimer = dashTime;
+            dashCooldownTimer = dashCooldown;
+            rb.AddForce(move.normalized * moveSpeed * 2.0f, ForceMode2D.Impulse);
+        }
+    }
+
+    private bool AbleToMove()
+    {
+        if (dashCooldownTimer <= 0.0f)
+        {
+            return true;
+        }
+        return false;
     }
 }
--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -5,21 +5,45 @@ public class PlayerInputHandler : MonoBehaviour {
 
     private PlayerController playerController;
     private Vector2 move;
+    private bool attack;
+    private bool dash;
 
 	void Start () {
         playerController = FindObjectOfType<PlayerController>();
 	}
 	
+    void Update()
+    {
+        attack = Input.GetButtonDown("Fire1");
+        dash = Input.GetButtonDown("Jump");
+
+        if (attack)
+            playerController.Attack();
+        if (dash)
+            playerController.Dash(move);
+    }
+
 	void FixedUpdate() {
         move = Vector2.zero;
 
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        //Keyboard controls are digital, getting keys here will avoid analogue emulation
-
-
         move = v * Vector2.up + h * Vector2.right;
         playerController.Move(move);
+    }
+
+    //Keyboard controls are digital, getting keys here will avoid analogue emulation
+    //Currently unused..
+    private void DigitalInput(float h, float v)
+    {
+        if (Input.GetKey(KeyCode.W))
+            v = 1.0f;
+        else if (Input.GetKey(KeyCode.S))
+            v = -1.0f;
+        if (Input.GetKey(KeyCode.A))
+            h = -1.0f;
+        else if (Input.GetKey(KeyCode.D))
+            h = 1.0f;
     }
 }
