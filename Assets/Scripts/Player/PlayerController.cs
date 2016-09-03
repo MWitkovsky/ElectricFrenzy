791e07b47bf2a458b52a0fdd90adad34aa24d665
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-09-03 10:09:59 -0400

    gitignore and character controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	public void Move(Vector2 move)
+    {
+        transform.Translate(move * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputHandler : MonoBehaviour {
+
+    private PlayerController playerController;
+    private Vector2 move;
+
+	void Start () {
+        playerController = FindObjectOfType<PlayerController>();
+	}
+	
+	void FixedUpdate() {
+        move = Vector2.zero;
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //Keyboard controls are digital, getting keys here will avoid analogue emulation
+
+
+        move = v * Vector2.up + h * Vector2.right;
+        playerController.Move(move);
+    }
+}
