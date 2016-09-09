c2ae7ef3c968543a607635271fae3d01d83963cb
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-09-09 13:01:23 -0400

    Added Frenzy Bar Removed label from health bar Added editable speeds for health bar effects Refactored some variable names in health bar code

---
+++ Assets/Scripts/UI/FrenzyBar.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FrenzyBar : MonoBehaviour
+{ 
+    [SerializeField]
+    private GameObject fillGraphic;
+    [SerializeField]
+    private float fillSpeed;
+
+    private Image frenzyBarImage;
+    private float charge, displayCharge, targetDisplayCharge;
+    private bool lerp;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (fillGraphic == null)
+            fillGraphic = GameObject.Find("FrenzyFill");
+
+        frenzyBarImage = fillGraphic.GetComponent<Image>();
+        charge = 0.0f;
+    }
+
+    void Update()
+    {
+        if (lerp)
+        {
+            displayCharge = Mathf.Lerp(displayCharge, targetDisplayCharge, fillSpeed * Time.deltaTime);
+            if(Mathf.Abs(displayCharge - targetDisplayCharge) < 0.01f)
+            {
+                displayCharge = targetDisplayCharge;
+                lerp = false;
+            }
+        }
+
+        updateGraphics();
+
+        //DEBUG CONTROLS
+        if (GameManager.IsDebugEnabled())
+            ProcessDebugControls();
+    }
+
+    private void AddFrenzyCharge(float charge)
+    {
+        this.charge += charge;
+        if (this.charge > 100.0f)
+            this.charge = 100.0f;
+
+        targetDisplayCharge = this.charge / 100.0f;
+        lerp = true;
+    }
+
+    private void RemoveFrenzyCharge(float charge)
+    {
+        this.charge -= charge;
+        if (this.charge < 0.0f)
+            this.charge = 0.0f;
+
+        targetDisplayCharge = this.charge / 100.0f;
+        lerp = true;
+    }
+
+    private void ProcessDebugControls()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+            RemoveFrenzyCharge(20.0f);
+
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+            AddFrenzyCharge(20.0f);
+    }
+
+    private void updateGraphics()
+    {
+        frenzyBarImage.fillAmount = displayCharge;
+    }
+}
