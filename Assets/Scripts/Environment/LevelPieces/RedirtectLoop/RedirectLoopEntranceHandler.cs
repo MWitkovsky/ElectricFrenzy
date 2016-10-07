fc3a30cfccb258b7d05a94af921c420c94f24c19
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-10-07 02:25:16 -0400

    Fixed lighting for all scenes Finished redirect loop Made prefabs for bandwidth pipe and redirect loop

---
+++ Assets/Scripts/Environment/LevelPieces/RedirtectLoop/RedirectLoopEntranceHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedirectLoopEntranceHandler : MonoBehaviour {
+
+    private RedirectLoopMain main;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(TagManager.Player))
+            main.SetPlayer(other.transform);
+    }
+
+    public void SetMain(RedirectLoopMain main)
+    {
+        this.main = main;
+    }
+}
