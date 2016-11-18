8979f2b01f0fcc9e3ff60647a9d36862c45afaff
Alec Day <shirojyuu@gmail.com>
2016-11-18 11:20:25 -0500

    Cleaned up worm particles

--- Assets/Scripts/Entities/Enemies/Worm/WormMain.cs
+++ Assets/Scripts/Entities/Enemies/Worm/WormMain.cs
@@ -200,7 +200,7 @@ public class WormMain : MonoBehaviour {
 
     public void TakeDamage(int damage)
     {
-        Instantiate(Resources.Load(ResourcePaths.SmallHitPrefab), transform.position, Quaternion.identity);
+        Instantiate(Resources.Load(ResourcePaths.SmallHitPrefab), transform.GetChild(0).position, Quaternion.identity);
         health -= damage;
         if (health <= 0)
         {
