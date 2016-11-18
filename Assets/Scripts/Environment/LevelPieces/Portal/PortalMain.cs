a40639ac4e31735fa533ca64162bf601ef31f15b
Alec Day <shirojyuu@gmail.com>
2016-11-18 10:17:28 -0500

    Created Portal object

---
+++ Assets/Scripts/Environment/LevelPieces/Portal/PortalMain.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0.0f, 9.5f * Time.deltaTime, 0.0f));
+	}
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if(obj.gameObject.tag.Equals("Player"))
+        {
+            
+        }
+    }
+}
