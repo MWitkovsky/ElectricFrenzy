cb173bb269b46d33654daadb6118d69949c9c24b
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-12-11 23:33:25 -0500

    Added enemy doors - read comments at top of EnemyDoor.cs for usage Edited layout of Calibration-3 to include these new doors

---
+++ Assets/Scripts/Environment/Traps/EnemyDoor/EnemyDoor.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDoor : MonoBehaviour {
+
+    //Mark this field if you want doors to not exist until the sensor is entered. If not turned on, then the sensor is pointless and is safe to delete.
+    [SerializeField] private bool disableDoorsOnStart;
+    //Put all doors here.
+    [SerializeField] private GameObject[] doors;
+    //Put all enemies tied to the door here.
+    [SerializeField] private GameObject[] enemies;
+
+    private float checkDelay = 0.5f, checkTimer;
+
+	void Start () {
+        if (disableDoorsOnStart)
+        {
+            for (int i = 0; i < doors.Length; i++)
+            {
+                doors[i].SetActive(false);
+            }
+        }
+        
+        checkTimer = checkDelay;
+	}
+	
+	void Update () {
+	    if(checkTimer > 0.0f)
+        {
+            checkTimer -= Time.deltaTime;
+        }
+        else
+        {
+            for(int i=0; i<enemies.Length; i++)
+            {
+                if (enemies[i] != null)
+                    break;
+                else if (i == enemies.Length - 1)
+                    OpenDoor();
+            }
+            checkTimer = checkDelay;
+        }
+	}
+
+    private void OpenDoor()
+    {
+        for (int i = 0; i < doors.Length; i++)
+        {
+            //spawn particle effect on door or something
+        }
+        Destroy(gameObject); //should delete all the doors with it
+    }
+
+    public void LockDoor()
+    {
+        for (int i = 0; i < doors.Length; i++)
+        {
+            //spawn particle effect on door or something
+            doors[i].SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/Traps/EnemyDoor/EnemyDoorSensor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDoorSensor : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(TagManager.Player))
+        {
+            transform.parent.GetComponent<EnemyDoor>().LockDoor();
+            Destroy(gameObject);
+        }
+    }
+}
