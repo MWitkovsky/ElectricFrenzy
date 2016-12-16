f0189abeed9ac75d9563706ba3526b2647c176bf
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-12-15 22:39:04 -0500

    logic done mostly

---
+++ Assets/Scripts/UI/VictoryScreen/RankLabel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RankLabel : MonoBehaviour {
+
+    [Tooltip("S - D")]
+    public Sprite[] rankImages = new Sprite[5];
+
+	public void SetImage(int index)
+    {
+        GetComponent<Image>().sprite = rankImages[index];
+    }
+}
